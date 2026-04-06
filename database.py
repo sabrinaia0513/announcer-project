55de82e1b5edd49a90ba7766e36cf7c266229f9e
sabrinaia0513 <sabrinaia0513@gmail.com>
2026-04-06 12:09:44 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
